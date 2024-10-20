9c0c1151b704d3cd0459377c7d6b95145a3bb8fc
thissya <thissya129@gmail.com>
2024-10-21 02:47:48 +0530

    domain O Env2

--- Backend/Flask/ocr_utils.py
+++ Backend/Flask/ocr_utils.py
@@ -2,7 +2,7 @@ from PIL import Image
 import pytesseract
 
 def extract_text_from_image(image_path):
-    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+    #pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
     image = Image.open(image_path)
     text = pytesseract.image_to_string(image)
     return text
